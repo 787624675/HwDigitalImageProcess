5b390f07b2e6b2b608f6b8e82def3a1034366262
787624675 <787624675@qq.com>
2021-11-17 17:40:18 +0800

    缩小的精度实现了

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -13,8 +13,13 @@ import time
 def judge(img1, img2, ratio):
     img1 = img1.astype(np.int32)
     img2 = img2.astype(np.int32)
+    with open('img1.txt', 'wt') as f:
+        print(img1, file=f)
+    with open('img2.txt', 'wt') as f:
+        print(img2, file=f)
 
     diff = np.abs(img1 - img2)
+    print(diff)
     count = np.sum(diff > 1)
 
     assert count == 0, f'ratio={ratio}, Error!'
@@ -28,16 +33,48 @@ def get_gt(img, ratio):
 
     return gt
 
+def resize(src_img, ratio):
+    """
+    双线性插值法,来调整图片尺寸
+    :param org_img: 原始图片
+    :param dst_shape: 调整后的目标图片的尺寸
+    :return:    返回调整尺寸后的图片矩阵信息
+    """
+    scrH, scrW, _ = img.shape
+    if(ratio < 1):
+        dst_shape = [int(scrH * ratio), int(scrW * ratio)]
+    else:
+        dst_shape = [int(scrH * ratio), int(scrW * ratio)]
+    dst_img = np.zeros((dst_shape[0], dst_shape[1], 3), np.int32)
+    dst_h, dst_w = dst_shape
+    src_h = src_img.shape[0]
+    src_w = src_img.shape[1]
+    # i：纵坐标y，j：横坐标x
+    # 缩放因子，dw,dh
+    scale_w = src_w / dst_w
+    scale_h = src_h / dst_h
 
-# to do
-def resize(img, ratio):
-    """
-    禁止使用cv2、torchvision等视觉库
-    type img: ndarray(uint8)
-    type ratio: float
-    rtype: ndarray(uint8)
-    """
-    pass
+    for i in range(dst_h):
+        for j in range(dst_w):
+            src_x = float((j + 0.5) * scale_w - 0.5)
+            src_y = float((i + 0.5) * scale_h - 0.5)
+			
+			# 向下取整，代表靠近源点的左上角的那一点的行列号	
+            src_x_int = math.floor(src_x)
+            src_y_int = math.floor(src_y)
+            
+            # 取出小数部分，用于构造权值
+            src_x_float = src_x - src_x_int
+            src_y_float = src_y - src_y_int
+
+            if src_x_int + 1 == src_w or src_y_int + 1 == src_h:
+                dst_img[i, j, :] = src_img[src_y_int, src_x_int, :]
+                continue
+            dst_img[i, j, :] = (1. - src_y_float) * (1. - src_x_float) * src_img[src_y_int, src_x_int, :] + \
+                               (1. - src_y_float) * src_x_float * src_img[src_y_int, src_x_int + 1, :] + \
+                               src_y_float * (1. - src_x_float) * src_img[src_y_int + 1, src_x_int, :] + \
+                               src_y_float * src_x_float * src_img[src_y_int + 1, src_x_int + 1, :]
+    return dst_img
 
 
 def show_images(img1, img2):
@@ -51,16 +88,19 @@ def show_images(img1, img2):
 if __name__ == '__main__':
     ratios = [0.5, 0.8, 1.2, 1.5]
 
-    img = cv2.imread('images/img_1.jpeg')   # type(img) = ndarray, 一共有三张图片，都可以尝试
+    # type(img) = ndarray, 一共有三张图片，都可以尝试
+    img = cv2.imread('images/img_1.jpeg')
+
+    print(img.shape)
 
     start_time = time.time()
     for ratio in ratios:
         gt = get_gt(img, ratio)
         resized_img = resize(img, ratio)
-
+        show_images(gt, resized_img)  # added
         judge(gt, resized_img, ratio)
     end_time = time.time()
     total_time = end_time - start_time
-    
+
     print(f'用时{total_time:.4f}秒')
     print('Pass')
