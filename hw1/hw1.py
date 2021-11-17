357a77032a824a826eadcfc4d6eee55cd059c933
787624675 <787624675@qq.com>
2021-11-17 21:07:32 +0800

    放大的精度更高了一些

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -33,6 +33,45 @@ def get_gt(img, ratio):
 
     return gt
 
+def resize2(src_img, ratio):
+    srcH, srcW, _ = src_img.shape
+    dst_shape = [int(srcH * ratio), int(srcW * ratio)]
+    dst_img = np.zeros((dst_shape[0], dst_shape[1], 3), np.uint8)
+    dst_h, dst_w = dst_shape
+    src_h = src_img.shape[0]
+    src_w = src_img.shape[1]
+    scale_x = src_w / dst_w
+    scale_y = src_h / dst_h
+    for j in range(dst_h):
+        fy = (float)((j + 0.5) * scale_y - 0.5)
+        sy = np.floor(fy)
+        fy = fy - sy
+        sy = sy if sy < srcH - 2 else srcH-2
+        sy = sy if sy > 0 else 0
+        cbufy = [0,0]
+        cbufy[0] = np.uint8((1. - fy) * 2048)
+        cbufy[1] = 2048 - cbufy[0]
+        for i in range(dst_w):
+            fx = (float)((i + 0.5) * scale_x - 0.5)
+            sx = np.floor(fx)
+            fx = fx - sx
+            if (sx < 0):
+                fx = 0
+                sx = 0
+            if (sx >= srcW - 1):
+                fx = 0
+                sx = srcW - 2
+            cbufx = [0,0]
+            cbufx[0] = np.uint8((1. - fx) * 2048)
+            cbufx[1] = 2048 - cbufx[0]
+            for k in range(0,3):
+                dst_img[j, i, k] = (src_img[sy,sx,k] * cbufx[0] * cbufy[0] + \
+                    src_img[sy,sx+1,k] * cbufx[0] * cbufy[1] + \
+                    src_img[sy+1,sx,k] * cbufx[1] * cbufy[0] + \
+                    src_img[sy+1,sx+1,k] * cbufx[1] * cbufy[1])>>22
+    return dst_img
+
+
 def resize(src_img, ratio):
     """
     双线性插值法,来调整图片尺寸
@@ -40,36 +79,50 @@ def resize(src_img, ratio):
     :param dst_shape: 调整后的目标图片的尺寸
     :return:    返回调整尺寸后的图片矩阵信息
     """
-    scrH, scrW, _ = img.shape
+    scrH, scrW, _ = src_img.shape
     if(ratio < 1):
         dst_shape = [int(scrH * ratio), int(scrW * ratio)]
     else:
         dst_shape = [int(scrH * ratio), int(scrW * ratio)]
-    dst_img = np.zeros((dst_shape[0], dst_shape[1], 3), np.int32)
+    dst_img = np.zeros((dst_shape[0], dst_shape[1], 3), np.uint8)
     dst_h, dst_w = dst_shape
     src_h = src_img.shape[0]
     src_w = src_img.shape[1]
     # i：纵坐标y，j：横坐标x
     # 缩放因子，dw,dh
-    scale_w = src_w / dst_w
-    scale_h = src_h / dst_h
+    scale_w = float(src_w / dst_w)
+    scale_h = float(src_h / dst_h)
 
     for i in range(dst_h):
         for j in range(dst_w):
             src_x = float((j + 0.5) * scale_w - 0.5)
             src_y = float((i + 0.5) * scale_h - 0.5)
-			
 			# 向下取整，代表靠近源点的左上角的那一点的行列号	
             src_x_int = math.floor(src_x)
             src_y_int = math.floor(src_y)
-            
             # 取出小数部分，用于构造权值
             src_x_float = src_x - src_x_int
             src_y_float = src_y - src_y_int
 
-            if src_x_int + 1 == src_w or src_y_int + 1 == src_h:
-                dst_img[i, j, :] = src_img[src_y_int, src_x_int, :]
-                continue
+            if (src_x_int < 0):
+                src_x_float = 0
+                src_x_int = 0
+            if (src_x_int >= src_w - 1):
+                src_x_float = 0
+                src_x_int = src_w - 2
+
+            if (src_y_int < 0):
+                src_y_float = 0
+                src_y_int = 0
+            if (src_y_int >= src_h - 1):
+                src_y_float = 0
+                src_y_int = src_h - 2
+
+            # if src_x_int + 1 == src_w or src_y_int + 1 == src_h:
+            #     dst_img[i, j, :] = src_img[src_y_int, src_x_int, :]
+            #     continue
+            # cbufx[0] = np.uint8((1. - fx) * 2048)
+            # cbufx[1] = 2048 - cbufx[0]
             dst_img[i, j, :] = (1. - src_y_float) * (1. - src_x_float) * src_img[src_y_int, src_x_int, :] + \
                                (1. - src_y_float) * src_x_float * src_img[src_y_int, src_x_int + 1, :] + \
                                src_y_float * (1. - src_x_float) * src_img[src_y_int + 1, src_x_int, :] + \
