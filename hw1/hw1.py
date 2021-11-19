df9777f284de132eb1915b0efd8c1519c5c166bf
787624675 <787624675@qq.com>
2021-11-19 11:21:12 +0800

    测了一下，发现整数优化后更慢

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -34,7 +34,49 @@ def get_gt(img, ratio):
 
     return gt
 
+def resize3(src_img, ratio):
+    scrH, scrW, _ = src_img.shape
+    dst_shape = [int(scrH * ratio), int(scrW * ratio)]
+    dst_img = np.zeros((dst_shape[0], dst_shape[1], 3), np.uint8)
+    dst_h, dst_w = dst_shape
+    src_h = src_img.shape[0]
+    src_w = src_img.shape[1]
+    # i：纵坐标y，j：横坐标x
+    # 缩放因子，dw,dh
+    scale_w = float(src_w / dst_w)
+    scale_h = float(src_h / dst_h)
 
+    mkernel = 2048
+    for i in range(dst_h):
+        for j in range(dst_w):
+            src_x = float((j + 0.5) * scale_w - 0.5)
+            src_y = float((i + 0.5) * scale_h - 0.5)
+			# 向下取整，代表靠近源点的左上角的那一点的行列号	
+            src_x_int = math.floor(src_x)
+            src_y_int = math.floor(src_y)
+            # 取出小数部分，用于构造权值
+            src_x_float = (src_x - src_x_int)
+            src_y_float = (src_y - src_y_int)
+
+            if (src_x_int < 0):
+                src_x_float = 0.
+                src_x_int = 0
+            if (src_x_int >= src_w - 1):
+                src_x_float = 1.
+                src_x_int = src_w - 2
+
+            if (src_y_int < 0):
+                src_y_float = 0.
+                src_y_int = 0
+            if (src_y_int >= src_h - 1):
+                src_y_float = 1.
+                src_y_int = src_h - 2
+
+            dst_img[i, j, :] = (int((1. - src_y_float)*mkernel) * int((1. - src_x_float)*mkernel) * src_img[src_y_int, src_x_int, :] + \
+                               int((1. - src_y_float)*mkernel) * int(src_x_float*mkernel) * src_img[src_y_int, src_x_int + 1, :] + \
+                               int(src_y_float*mkernel) * int((1. - src_x_float)*mkernel) * src_img[src_y_int + 1, src_x_int, :] + \
+                               int(src_y_float*mkernel) * int(src_x_float*mkernel) * src_img[src_y_int + 1, src_x_int + 1, :]) >> 22
+    return dst_img
 
 def resize2(src_img, ratio):
     srcH, srcW, _ = src_img.shape
@@ -45,8 +87,8 @@ def resize2(src_img, ratio):
     src_w = src_img.shape[1]
     scale_x = src_w / dst_w
     scale_y = src_h / dst_h
-    mkernel = 128
-    step = 14
+    mkernel = 2048
+    step = 22
     cbufx = [0,0]
     cbufy = [0,0]
     for j in range(dst_h):
@@ -59,9 +101,10 @@ def resize2(src_img, ratio):
         if (sy >= src_h - 1):
             fy = 1.
             sy = src_h - 2
-        
-        cbufy[0] = math.floor((1. - fy) * mkernel)
-        cbufy[1] = mkernel - cbufy[0]
+        # cbufy[0] = math.floor((1. - fy) * mkernel)
+        # cbufy[1] = mkernel - cbufy[0]
+        cbufy[0] = (1. - fy) 
+        cbufy[1] = fy
         for i in range(dst_w):
             fx = ((i + 0.5) * scale_x - 0.5)
             sx = math.floor(fx)
@@ -72,15 +115,13 @@ def resize2(src_img, ratio):
             if (sx >= src_w - 1):
                 fx = 1.
                 sx = src_w - 2
-            
-            cbufx[0] = math.floor((1. - fx) * mkernel)
-            cbufx[1] = mkernel - cbufx[0]
-          
+            cbufx[0] = (1. - fx)
+            cbufx[1] = fx
             for k in range(0,3):
                 dst_img[j, i, k] = (src_img[sy,sx,k] * cbufx[0] * cbufy[0] + \
                     src_img[sy,sx+1,k] * cbufx[0] * cbufy[1] + \
                     src_img[sy+1,sx,k] * cbufx[1] * cbufy[0] + \
-                    src_img[sy+1,sx+1,k] * cbufx[1] * cbufy[1])  >> step
+                    src_img[sy+1,sx+1,k] * cbufx[1] * cbufy[1])  # >> step
     return dst_img
 
 
@@ -150,7 +191,7 @@ if __name__ == '__main__':
     start_time = time.time()
     for ratio in ratios:
         gt = get_gt(img, ratio)
-        resized_img = resize2(img, ratio)
+        resized_img = resize(img, ratio)
         show_images(gt, resized_img)  # added
         judge(gt, resized_img, ratio)
     end_time = time.time()
